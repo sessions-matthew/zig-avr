ee246dffd10b157b07204883ffe8af421106449c
sessions-matthew <sessions.matthew.j@gmail.com>
2021-11-29 11:58:15 -0800

    add arduino nano

---
+++ atmega328p.zig
@@ -0,0 +1,26 @@
+pub const atmega328p = extern struct {
+    fn SFR(comptime addr: u8) u8 {
+        return 0x20 + addr;
+    }
+    fn IO(comptime addr: u8) *u8 {
+        return @intToPtr(*u8, SFR(addr));
+    }
+
+    // TODO: Fill in all other registers from reference manual
+
+    pub const PORTB = IO(0x05);
+    pub const DDRB = IO(0x04);
+    pub const PINB = IO(0x03);
+
+    pub const Pin = packed struct { port: *u8 = PORTB, pin: *u8 = PINB, ddr: *u8 = DDRB, n: u8 };
+
+    pub const PB0 = Pin{ .n = 0 };
+    pub const PB1 = Pin{ .n = 1 };
+    pub const PB2 = Pin{ .n = 2 };
+    pub const PB3 = Pin{ .n = 3 };
+    pub const PB4 = Pin{ .n = 4 };
+    pub const PB5 = Pin{ .n = 5 };
+};
+comptime {
+    @export(atmega328p, .{ .name = "atmega328p", .linkage = .Strong });
+}
